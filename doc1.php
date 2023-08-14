4e6d84b3d2f36bed23dfc00351f31c3de5924d6e
Hassan Khani <h.khani@RASTAPAD.LOCAL>
2023-08-14 09:32:34 +0330

    Second coommit

--- doc1.php
+++ doc1.php
@@ -1,3 +1,4 @@
 key <a href="http://google.com">$_POST sapi_windows_cp_conv</a>
 //i do not know php soo do stuff randomnly
+//so there is anoder branch, i dont know why i'm doing this "learning git, but can't write code"
 end
